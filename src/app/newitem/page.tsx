60c6f4cdcedf69c82503115a016190b1cde21a70
2022JanikOndrej <ondra.janik24@gmail.com>
2024-04-18 12:52:44 +0200

    ahoj

---
+++ src/app/newitem/page.tsx
@@ -0,0 +1,54 @@
+import {prisma} from "../db";
+import Link from "next/link";
+import {redirect} from "next/navigation";
+
+export default function NewItem() {
+
+
+    async function createTodoItem(data: FormData){
+        "use server";
+        const title = data.get("title")?.valueOf();
+
+        if (typeof title === "string" ) {
+            await prisma.todo.create({
+                data:{title},
+            });
+
+            redirect('/');
+        }else return new Error("Invalid Title");
+
+    }
+    return (
+        <main className="">
+            <h2 className="text-2xl">Create new item</h2>
+            <form action={createTodoItem} className="flex gap-2 flex-col">
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Create a new todo item"
+                    className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 focus-within:border-zinc-700 bg-transparent outline-none transition duration-300 ease-in-out"
+
+
+                ></input>
+
+                <div className="flex gap-1 justify-end">
+                    <Link
+                        href=".."
+                        className="border border-slate-300 text-black rounded px-2 py-1 hover:bg-zinc-700 focus:bg-zinc-700 focus:border-zinc-700 outline-none font-bold"
+                    >
+                        Cancel
+                    </Link>
+
+                    <button
+                        type="submit"
+                        className="border border-slate-300 text-black rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 focus-within:border-zinc-700 transition-colors duration-300 outline-none font-bold"
+                    >
+                        Save
+                    </button>
+                </div>
+
+
+            </form>
+        </main>
+    );
+}
---
+++ src/zaza/card.tsx
@@ -0,0 +1,10 @@
+export default function Card(){
+    return (
+    <main className="bg-green-300">
+        <div className="text-3xl text-red-800">
+            <h1>Todo app č1</h1>
+        </div>
+    </main>
+    );
+}
+
